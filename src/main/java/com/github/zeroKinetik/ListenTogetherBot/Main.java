f2478dcffaefc6d2d0e884164f62fe7413e2812d
ZeroKinetik <hemansakkthivelms14@gmail.com>
2025-03-10 18:34:35 +0530

    finished loading

--- src/main/java/com/github/zeroKinetik/ListenTogetherBot/Main.java
+++ src/main/java/com/github/zeroKinetik/ListenTogetherBot/Main.java
@@ -1,7 +1,15 @@
 package com.github.zeroKinetik.ListenTogetherBot;
 
+import net.dv8tion.jda.api.JDABuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws LoginException {
+        Dotenv dotenv = Dotenv.load();
+        final String BOT_TOKEN = dotenv.get("DISCORD_TOKEN");
+        JDABuilder jdaBuilder = JDABuilder.createDefault(BOT_TOKEN);
+        jdaBuilder.build();
     }
 }
